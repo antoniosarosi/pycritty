e84070551d3a3c0fa68417182203762e54cd5f2d
Antonio Sarosi <sarosiantonio@gmail.com>
2020-12-13 18:11:58 +0100

    Add version

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ from alacritty import Alacritty, ConfigError
 import log
 
 
+VERSION = 'v0.1.0'
+
+
 def args() -> Dict[str, Any]:
     parser = argparse.ArgumentParser(
         prog='pycritty',
@@ -18,6 +21,7 @@ def args() -> Dict[str, Any]:
         formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=80),
         argument_default=argparse.SUPPRESS,
     )
+    parser.add_argument('-v', '--version', action='version', version=VERSION)
     parser.add_argument('-t', '--theme', metavar='"Theme name"')
     parser.add_argument('-f', '--font', metavar='"Font alias"')
     parser.add_argument('-s', '--size', type=float)
