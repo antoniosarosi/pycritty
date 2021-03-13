a4238819ac7796da3b37c7fa0d52856f45d288e0
Cyber Gsus <cyber.gsuscode@gmail.com>
2021-03-14 00:17:29 +0100

    Moved first-layer options to using click

--- pycritty/main.py
+++ pycritty/main.py
@@ -2,30 +2,17 @@
 # Antonio Sarosi
 # December 2020
 
-# from . import PycrittyError
-# from .io import log
-# from .commands import subcommands, Pycritty
-# from .cli import parser
-
 from .cli import pycritty_cli
-
-# def main():
-#     args = vars(parser.parse_args())
-#     if args['subcommand'] is None:
-#         command_receiver = Pycritty
-#     else:
-#         command_receiver = subcommands[args['subcommand']]
-#     args.pop('subcommand')
-#     try:
-#         command_receiver().execute(args)
-#     except (PycrittyError, KeyboardInterrupt) as e:
-#         if isinstance(e, KeyboardInterrupt):
-#             e = 'Interrupted, changes might not have been applied'
-#         log.err(e)
-#         exit(1)
+from .io import log
+from . import PycrittyError
 
 def main():
-    pycritty_cli()
+    try:
+        pycritty_cli()
+    except (PycrittyError, KeyboardInterrupt) as e:
+        if isinstance(e, KeyboardInterrupt):
+            e = 'Interrupted, changes might not have been applied'
+        log.err(e)
 
 if __name__ == '__main__':
     pycritty_cli()
